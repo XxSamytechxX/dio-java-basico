6b150da89cdce82153729de2ba3ecd14c4d27d06
XxSamytechxX <samueltech25@gmail.com>
2023-09-12 09:22:04 -0300

    fim das aulas sobre operadores

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+       
+     // classe Operadores.java
+int a, b;
+
+a = 6;
+b = 6;
+
+String resultado = (a==b) ? "verdadeiro" : "false";
+
+System.out.println(resultado);
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,7 +2,7 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-    final double VALOR_DE_PI = 3.14;
+    final double VALOR_DE_PI = 3.14; //Adicionar a palavra "final" ao início e escrever o nome da varíavel tudo em CAIXA ALTA para ter um valor constante que não pode ser alterado
 
     VALOR_DE_PI = 10.14;
 
