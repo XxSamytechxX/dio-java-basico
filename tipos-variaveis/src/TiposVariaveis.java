73d26bd93d313ce756a668121b4ca4c6cb3acf04
XxSamytechxX <samueltech25@gmail.com>
2023-09-12 08:29:13 -0300

    finalizando estudo sobre tipos e variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+    final double VALOR_DE_PI = 3.14;
+
+    VALOR_DE_PI = 10.14;
+
+        System.out.println(VALOR_DE_PI);
+    }
+    
+}
